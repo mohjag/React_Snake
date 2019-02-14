6b610ab343b9782495d104ae675bc0e19cb7d64c
Jagadeesh <jagadeesh.mohan@aspiresys.com>
2019-02-14 22:50:46 +0530

    Added Search Project.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class MainApp extends React.Component {
+
+    render() {
+        return ( <div>
+          
+            Welcome :)
+        </div> )
+    }
+}
+
+ReactDOM.render( <MainApp />, document.getElementById("main") ) 
+
+
